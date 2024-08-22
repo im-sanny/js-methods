2a15e209e33546c9fd85773015c55853255fc7a6
im-sanny <raselparvezsanny@gmail.com>
2024-08-22 19:02:20 +0600

    splice method added

--- slice.js
+++ slice.js
@@ -1,3 +1,3 @@
-const fruits =['apple', 'mango', 'cherry', 'blueberry'];
-const afterSlice= fruits.slice(1, 2, 3)
+const fruits = ["apple", "mango", "cherry", "blueberry"];
+const afterSlice = fruits.slice(1, 2, 3);
 console.log(afterSlice);
---
+++ splice.js
@@ -0,0 +1,4 @@
+const color = ["red", "blue", "white", "black", "gray"];
+const removed = color.splice(0, 2,'yellow')
+console.log(color);
+console.log(removed);
